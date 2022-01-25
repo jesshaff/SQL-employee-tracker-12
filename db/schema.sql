3cecdf2ea914ace8206092eab03cf06b743dd05c
jessicahaff@yahoo.com <jessicahaff@yahoo.com>
2022-01-24 21:19:32 -0500

    added department, role & employee tables to schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,16 +1,28 @@
-department
-id: INT PRIMARY KEY
-name: VARCHAR(30) to hold department name
+DROP DATABASE IF EXISTS employees_db;
+CREATE DATABASE employees_db;
 
-role
-id: INT PRIMARY KEY
-title: VARCHAR(30) to hold role title
-salary: DECIMAL to hold role salary
-department_id: INT to hold reference to department role belongs to
+USE employees_db;
 
-employee
-id: INT PRIMARY KEY
-first_name: VARCHAR(30) to hold employee first name
-last_name: VARCHAR(30) to hold employee last name
-role_id: INT to hold reference to employee role
-manager_id: INT to hold reference to another employee that is the manager of the current employee (null if the employee has no manager)
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    department_name VARCHAR(30) 
+);
+
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT,
+    FOREIGN KEY (department_id)
+    REFERENCES departments(id)
+);
+
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT,
+    PRIMARY KEY (id)
+);
+
