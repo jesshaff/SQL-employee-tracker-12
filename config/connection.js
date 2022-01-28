1a9b7b8e6653f8989ae10fdf17dd09ff23d9f442
jessicahaff@yahoo.com <jessicahaff@yahoo.com>
2022-01-27 23:51:56 -0500

    added cfonts for Employee Tracker text

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,6 @@
 const mysql2 = require('mysql2');
 const chalk = require('chalk');
+const cfonts = require('cfonts');
 
 connection = mysql2.createConnection( {
     host: 'localhost',
@@ -9,15 +10,20 @@ connection = mysql2.createConnection( {
     multipleStatements: true
 });
 
-console.log(chalk.bold.green(`
-╔═══╗─────╔╗──────────────╔═╗╔═╗
-║╔══╝─────║║──────────────║║╚╝║║
-║╚══╦╗╔╦══╣║╔══╦╗─╔╦══╦══╗║╔╗╔╗╠══╦═╗╔══╦══╦══╦═╗
-║╔══╣╚╝║╔╗║║║╔╗║║─║║║═╣║═╣║║║║║║╔╗║╔╗╣╔╗║╔╗║║═╣╔╝
-║╚══╣║║║╚╝║╚╣╚╝║╚═╝║║═╣║═╣║║║║║║╔╗║║║║╔╗║╚╝║║═╣║
-╚═══╩╩╩╣╔═╩═╩══╩═╗╔╩══╩══╝╚╝╚╝╚╩╝╚╩╝╚╩╝╚╩═╗╠══╩╝
-───────║║──────╔═╝║─────────────────────╔═╝║
-───────╚╝──────╚══╝─────────────────────╚══╝`));
+cfonts.say('Employee Tracker', {
+    font: 'block',                      // define the font face
+	align: 'left',                      // define text alignment
+	colors: ['white'],
+    gradient: ['cyan', '#A020F0'],      // define all colors
+	background: 'transparent',          // define the background color, you can also use `backgroundColor` here as key
+	letterSpacing: 1,                   // define letter spacing
+	lineHeight: 0,                      // define the line height
+	space: true,                        // define if the output text should have empty lines on top and on the bottom
+	maxLength: '8',                     // define how many character can be on one line
+	independentGradient: true,          // define if you want to recalculate the gradient for each new line
+	transitionGradient: true,           // define if this is a transition between colors directly
+	env: 'node'                         // define the environment CFonts is being executed in
+});
 
 // Connects to sql server and sql database
 connection.connect((err) => {
