45b6b8097f4a72d9c686e67532957b36048954c9
jessicahaff@yahoo.com <jessicahaff@yahoo.com>
2022-01-28 00:20:25 -0500

    added comments to connection.js

--- config/connection.js
+++ config/connection.js
@@ -2,6 +2,7 @@ const mysql2 = require('mysql2');
 const chalk = require('chalk');
 const cfonts = require('cfonts');
 
+// Creating connection to mysql2
 connection = mysql2.createConnection( {
     host: 'localhost',
     port: 3306,
@@ -10,16 +11,17 @@ connection = mysql2.createConnection( {
     multipleStatements: true
 });
 
-cfonts.say('Employee Tracker', {
+// Employee Tracker "Welcome Message"
+cfonts.say('Employee Tracker!', {
     font: 'block',                      // define the font face
 	align: 'left',                      // define text alignment
-	colors: ['white'],
-    gradient: ['cyan', '#A020F0'],      // define all colors
+	colors: ['white'],                  // define all colors
+    gradient: ['cyan', '#A020F0'],      // define your two gradient colors
 	background: 'transparent',          // define the background color, you can also use `backgroundColor` here as key
 	letterSpacing: 1,                   // define letter spacing
 	lineHeight: 0,                      // define the line height
 	space: true,                        // define if the output text should have empty lines on top and on the bottom
-	maxLength: '8',                     // define how many character can be on one line
+	maxLength: '9',                     // define how many character can be on one line
 	independentGradient: true,          // define if you want to recalculate the gradient for each new line
 	transitionGradient: true,           // define if this is a transition between colors directly
 	env: 'node'                         // define the environment CFonts is being executed in
